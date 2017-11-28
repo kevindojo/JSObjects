6bebc6739d9ab3e4483a86efd05de3fd851c435d
KevinPham <kevinpham15@gmail.com>
2017-11-28 13:33:43 -0800

    inital

---
+++ JSObjects.js
@@ -0,0 +1,11 @@
+let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
+
+
+console.log(students[name])
